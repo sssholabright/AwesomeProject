32e9fcf57bc6e6390798174a6a12d43ac8d17afa
Mujeeb <adejobimujeeb5@gmail.com>
2021-01-26 13:34:42 +0100

    initial commit

---
+++ Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Todo = ({ title }) => {
+    return (
+        <View>
+            <Text>
+                ✅ {title}
+            </Text>
+        </View>
+    )
+}
+
+export default Todo
+
+const styles = StyleSheet.create({})
